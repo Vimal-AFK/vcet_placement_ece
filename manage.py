0ec956e45c015897d84520cfddac12df0a3c6247
Vimal-AFK <vimalrajnagarajan147@gmail.com>
2025-01-12 17:40:20 +0530

    changed project name , vect_placement_ece --> vcet_placement_ece

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vect_placement_ece.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vcet_placement_ece.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- vcet_placement_ece/asgi.py
+++ vcet_placement_ece/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for vect_placement_ece project.
+ASGI config for vcet_placement_ece project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vect_placement_ece.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vcet_placement_ece.settings')
 
 application = get_asgi_application()
