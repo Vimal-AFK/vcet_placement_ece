9a8b6626df5b12cb8161d9691f03b11adf8c8cab
Vimal-AFK <vimalrajnagarajan147@gmail.com>
2025-01-12 17:08:37 +0530

    created a urls and register the app in settings

--- vect_placement_ece/settings.py
+++ vect_placement_ece/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2*$kr#s%3$j2w0ny7xw0(yr1u+@6y!j#znq=mt9&m77ydv$mz(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aptitude_app',
 ]
 
 MIDDLEWARE = [
